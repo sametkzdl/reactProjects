3bfe8897bf8fb83b30b88a755ef278d2532c7148
sametkzdl <sametkazdal_53@outlook.com>
2024-07-29 16:36:37 +0300

    randomCard yapıldı

--- src/pages/jokesGenerator/jokes.js
+++ src/pages/jokesGenerator/jokes.js
@@ -1,6 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const RandomCard = () => {
+  const [value, setValue] = useState("");
+  const [getRandomCard, setGetRandomCard] = useState(1);
+  const randomNumber = () => {
+    const number = Math.floor(Math.random() * 20);
+    return number;
+  };
+  useEffect(() => {
+    const getUsers = async () => {
+      setValue(false);
+      const { data } = await axios(
+        `https://fakestoreapi.com/products/${getRandomCard}`
+      );
+      setValue(data);
+    };
+    getUsers();
+  }, [getRandomCard]);
   return (
     <div
       style={{
@@ -12,7 +29,30 @@ const RandomCard = () => {
         marginBlock: "30px",
       }}
     >
-      <button>Get Random product Card</button>
+      {value ? (
+        <button onClick={() => setGetRandomCard(randomNumber())}>
+          Get Random Card
+        </button>
+      ) : (
+        <button onClick={() => alert("data is loading ... wait 3 seconds")}>
+          Loading
+        </button>
+      )}
+      {value ? (
+        <div>
+          <h2>{value.title}</h2>
+          <p>ID: {value.id}</p>
+          <p>Price: ${value.price}</p>
+          <p>Description: {value.description}</p>
+          <p>Category: {value.category}</p>
+          <img width={300} height={300} src={value.image} alt={value.title} />
+          <p>
+            Rating: {value.rating.rate} (Count: {value.rating.count})
+          </p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
