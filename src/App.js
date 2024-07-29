fd39beec8c2a6a110d2074bfa286677c8563537e
sametkzdl <sametkazdal_53@outlook.com>
2024-07-29 17:06:07 +0300

    header düzenlendi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { Routes, Route, useParams, Link, useLocation } from "react-router-dom";
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+import React from "react";
 import PureHtmlForm from "./pages/forms/purehtmlform";
 import ReactHookForm from "./pages/forms/reactHookForm";
 import Home from "./pages/home";
 import RandomCard from "./pages/randomCard/jokes";
-
+import Validator from "./pages/validator/validator";
 function App() {
   const location = useLocation();
+  let components = { Home, PureHtmlForm, ReactHookForm, RandomCard, Validator };
+
   const urlData = [
-    { name: "Home", href: "/" },
-    { name: "PureHtmlForm", href: "/PureHtmlForm" },
-    { name: "ReactHookForm", href: "/ReactHookForm" },
-    { name: "RandomCard", href: "/RandomCard" },
+    { name: "Home", href: "/", id: 1 },
+    { name: "PureHtmlForm", href: "/PureHtmlForm", id: 2 },
+    { name: "ReactHookForm", href: "/ReactHookForm", id: 3 },
+    { name: "RandomCard", href: "/RandomCard", id: 4 },
+    { name: "Validator", href: "/Validator", id: 5 },
   ];
   return (
     <div className="App">
@@ -62,10 +66,12 @@ function App() {
         </ul>
       </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/PureHtmlForm" element={<PureHtmlForm />} />
-        <Route path="/ReactHookForm" element={<ReactHookForm />} />
-        <Route path="/RandomCard" element={<RandomCard />} />
+        {urlData.map(({ name, href, id }) => {
+          const Page = components[name];
+          return (
+            <Route key={id} path={href} element={React.createElement(Page)} />
+          );
+        })}
       </Routes>
     </div>
   );
---
+++ src/pages/validator/validator.js
@@ -0,0 +1,9 @@
+const Validator = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Validator</h2>
+    </div>
+  );
+};
+
+export default Validator;
