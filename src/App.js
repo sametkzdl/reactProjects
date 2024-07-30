fd7789bbb63f336ec8f1b0e0474d6b77c5ab8991
sametkzdl <sametkazdal_53@outlook.com>
2024-07-30 13:05:54 +0300

    use callBack ve useMemo eklendi

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import React from "react";
 import PureHtmlForm from "./pages/forms/purehtmlform";
 import ReactHookForm from "./pages/forms/reactHookForm";
 import Home from "./pages/home";
 import RandomCard from "./pages/randomCard/jokes";
 import Validator from "./pages/validator/validator";
+import Lessons from "./pages/educationReact/lessons";
+import First from "./pages/educationReact/first";
+import Second from "./pages/educationReact/second";
+import Third from "./pages/educationReact/third";
+import Fourth from "./pages/educationReact/fourth";
 function App() {
-  const location = useLocation();
-  let components = { Home, PureHtmlForm, ReactHookForm, RandomCard, Validator };
-
   const urlData = [
     { name: "Home", href: "/", id: 1 },
     { name: "PureHtmlForm", href: "/PureHtmlForm", id: 2 },
     { name: "ReactHookForm", href: "/ReactHookForm", id: 3 },
     { name: "RandomCard", href: "/RandomCard", id: 4 },
     { name: "Validator", href: "/Validator", id: 5 },
+    { name: "Lessons", href: "/Lessons", id: 6 },
   ];
   return (
     <div className="App">
@@ -30,49 +33,49 @@ function App() {
             boxSizing: "border-box",
           }}
         >
-          {urlData.map(({ name, href }, index) => {
+          {urlData.map(({ name, href, id }) => {
             return (
-              <li key={index}>
-                {location.pathname === href ? (
-                  <Link
-                    to={href}
-                    style={{
-                      textDecoration: "none",
-                      border: "3px solid red",
-                      padding: "7px",
-                      borderRadius: "5px",
-                      height: "50px",
-                    }}
-                  >
-                    {name}
-                  </Link>
-                ) : (
-                  <Link
-                    to={href}
-                    style={{
-                      textDecoration: "none",
-                      border: "1px solid #eeeddd",
-                      padding: "7px",
-                      borderRadius: "5px",
-                      height: "50px",
-                    }}
-                  >
-                    {name}
-                  </Link>
-                )}
-              </li>
+              <NavLink
+                style={({ isActive, isPending, isTransitioning }) => {
+                  return {
+                    border: isActive ? "2px solid red" : "none",
+                    padding: isActive ? "5px" : "none",
+                    color: isPending ? "red" : "black",
+                    viewTransitionName: isTransitioning ? "slide" : "",
+                  };
+                }}
+                key={id}
+                to={href}
+              >
+                {name === "Lesson" ? null : name}
+              </NavLink>
             );
           })}
         </ul>
       </header>
       <Routes>
-        {urlData.map(({ name, href, id }) => {
-          const Page = components[name];
-          return (
-            <Route key={id} path={href} element={React.createElement(Page)} />
-          );
-        })}
+        <Route path="/" element={<Home />} />
+        <Route path="/PureHtmlForm" element={<PureHtmlForm />} />
+        <Route path="/ReactHookForm" element={<ReactHookForm />} />
+        <Route path="/RandomCard" element={<RandomCard />} />
+        <Route path="/Validator" element={<Validator />} />
+        <Route path="/Validator" element={<Validator />} />
+        <Route path="/Lessons" element={<Lessons />}>
+          <Route path="First" element={<First />} />
+          <Route path="Second" element={<Second />} />
+          <Route path="Third" element={<Third />} />
+          <Route path="Fourth" element={<Fourth />} />
+        </Route>
+        <Route path={"*"} element={<Error />} />
       </Routes>
+    </div>
+  );
+}
+
+export function Error() {
+  return (
+    <div>
+      <h2>This Page İs Not Found</h2>
     </div>
   );
 }
